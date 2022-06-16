120666c8eb35d4f594576e25c45800b6a5ae37f5
Divyanarina <divyanarina511@gmail.com>
2022-06-16 20:38:48 +0530

    Input read command execution

---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'Enter you name: ' name
+echo "hey $name. welcome to devops traning.."
+
+read -p 'Enter your number: ' phone
+echo "thank for entering will send details to $phone"
