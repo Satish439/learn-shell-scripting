9d8dc9a4b4b3097986406947edb4673c7188e4dc
Divyanarina <divyanarina511@gmail.com>
2022-07-26 17:21:25 +0530

    Ec2 lunch script

--- Special.var inputs.sh
+++ Special.var inputs.sh
@@ -5,3 +5,6 @@ echo first input = $1
 echo second input = $2
 echo All input = $*
 echo number of input = $#
+
+# From 10th value we need to access the varibles as ${10} instead of $10
+
--- shebang.sh
+++ shebang.sh
@@ -3,3 +3,16 @@
 ls
 
 cat/etc/passwd
+
+print "Hello World"
+
+# to print some message on screen then
+# Command: echo
+# Syntax: echo INPUT
+
+echo Hello world
+echo Hello, How are you doing today, Good day
+
+#Print multiple lines
+
+echo -e "Hello,\nHow are you doing today? \nGood day"
